fe0be5605f5185337d308ace561bc7ef1f5a907c
Maxattt <113494886+Maxattt@users.noreply.github.com>
2025-02-05 03:43:19 +0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+const TodoList = () => {
+    const items = ['Install React', 'Study React', 'Use React', 'Build React App'];
+    return (
+        <ul>
+            {items.map((item, index) => <li key={index}>{item}</li>)}
+        </ul>
+    );
+};
+
+const AppHeader = () => {
+    return <h1>My Todo List</h1>;
+};
+
+const SearchPanel = () => {
+    const searchText = 'Type here to search';
+    const searchStyle = {
+        fontSize: '20px',
+        padding: '10px'
+    };
+    return <input style={searchStyle} placeholder={searchText} />;
+};
+
+// App component
+const App = () => {
+    const value = '<script>alert ("")</script>';
+
+    return (
+        <div>
+            <AppHeader />
+            <SearchPanel />
+            <TodoList />
+        </div>
+    );
+};
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
